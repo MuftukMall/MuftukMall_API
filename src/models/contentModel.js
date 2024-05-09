0ab9f061381343a77a251640ca4561693792b897
Muftuk Mall <155059661+MuftukMall@users.noreply.github.com>
2024-05-09 23:13:12 +0530

    Update contentModel.js

--- src/models/contentModel.js
+++ src/models/contentModel.js
@@ -10,6 +10,15 @@ const contentSchema = new Schema({
   subject: { type: String },
 });
 
+// Update toJSON method to modify contentUrl
+contentSchema.methods.toJSON = function () {
+  const obj = this.toObject();
+  if (obj.contentUrl.includes(".m3u8")) {
+    obj.contentUrl = obj.contentUrl.replace(/https:\/\/.*?\.cloudfront\.net/g, "https://muftukmall-url.vercel.app");
+  }
+  return obj;
+};
+
 const contentModel = model("Content", contentSchema);
 
 export default contentModel;
