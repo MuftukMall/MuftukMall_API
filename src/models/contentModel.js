5538a20479d1423a76a72b4dbe80a9477f34fbec
Muftuk Mall <155059661+MuftukMall@users.noreply.github.com>
2024-06-18 11:33:35 +0530

    Update contentModel.js

--- src/models/contentModel.js
+++ src/models/contentModel.js
@@ -14,7 +14,7 @@ const contentSchema = new Schema({
 contentSchema.methods.toJSON = function () {
   const obj = this.toObject();
   if (obj.contentUrl.includes(".m3u8")) {
-    obj.contentUrl = obj.contentUrl.replace(/https:\/\/.*?\.cloudfront\.net/g, "https://pwurlapi.vercel.app");
+    obj.contentUrl = obj.contentUrl.replace(/https:\/\/.*?\.cloudfront\.net/g, "https://pwurlapi.vercel.app").replace(/https:\/\/.*?\.bitgravity\.com/g, "https://pwurlapi.vercel.app");
   }
   return obj;
 };
