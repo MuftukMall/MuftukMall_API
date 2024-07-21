c10b2107d11945d9baa8d374c57151a40b965525
Muftuk Mall <155059661+MuftukMall@users.noreply.github.com>
2024-07-21 12:02:33 +0530

    Update contentModel.js

--- src/models/contentModel.js
+++ src/models/contentModel.js
@@ -14,7 +14,7 @@ const contentSchema = new Schema({
 contentSchema.methods.toJSON = function () {
   const obj = this.toObject();
   if (obj.contentUrl.includes(".m3u8")) {
-    obj.contentUrl = obj.contentUrl.replace(/https:\/\/.*?\.cloudfront\.net/g, "https://muftukmall-mm.koyeb.app").replace(/https:\/\/.*?\.bitgravity\.com/g, "https://muftukmall-pw.koyeb.app");
+    obj.contentUrl = obj.contentUrl.replace(/https:\/\/.*?\.cloudfront\.net/g, `${process.env.NEW_URL}`).replace(/https:\/\/.*?\.bitgravity\.com/g, `${process.env.NEW_URL}`);
   }
   return obj;
 };
